9e5bfbfd1dc022d4c7aab57f0ff24874af53fc4e
gunjannn17 <gunjangurjar9826@gmail.com>
2026-08-31 23:30:48 +0530

    Time: 17 ms (78.03%) | Memory: 45.9 MB (14.63%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -7,37 +7,29 @@
 
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        // Step 1: Sort the array so we can use the two-pointer approach
-        Arrays.sort(nums);
-        int closestSum = nums[0] + nums[1] + nums[2]; // Initialize with the first possible sum
-        
-        // Step 2: Fix one number and use two pointers for the other two
-        for (int i = 0; i < nums.length - 2; i++) {
-            int left = i + 1;
-            int right = nums.length - 1;
-            
-            while (left < right) {
-                int currentSum = nums[i] + nums[left] + nums[right];
-                
-                // If we found an exact match, return it immediately
-                if (currentSum == target) {
-                    return currentSum;
-                }
-                
-                // Update closestSum if the current sum is closer to the target
-                if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
-                    closestSum = currentSum;
-                }
-                
-                // Move pointers based on how the current sum compares to the target
-                if (currentSum < target) {
-                    left++; // We need a larger sum
-                } else {
-                    right--; // We need a smaller sum
-                }
-            }
-        }
-        
-        return closestSum;
+     Arrays.sort(nums);
+     int closestSum=nums[0]+nums[1]+nums[2];
+     for(int i=0;i<nums.length-2;i++){
+       int left=i+1;
+      int right=nums.length-1;
+     
+while(left<right){
+  int  currentSum=nums[i]+nums[left]+nums[right];
+
+
+if(currentSum==target){
+    return currentSum;
+}
+if(Math.abs(currentSum-target)<Math.abs(closestSum-target)){
+    closestSum=currentSum;
+}
+if(currentSum<target){
+    left++;
+}else{
+    right--;
+}
+}
+     }
+  return closestSum;    
     }
 }
